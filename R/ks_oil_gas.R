55ca1714e47fa90e00e5c4359275806ee52d36d3
Stephen Johnson <stephenjjohnson@gmail.com>
2016-03-16 10:35:52 -0500

    Update README.md

--- R/ks_oil_gas.R
+++ R/ks_oil_gas.R
@@ -1,5 +1,8 @@
 # Download and combine annual data for oil and gas production from
 # the Kansas Geological Survey at http://www.kgs.ku.edu/PRS/petro/state.html
+#
+# Saves both original files from the website and the processed data.
+#
 # Author: Stephen Johnson
 # Licence: GPL-2
 
@@ -14,9 +17,9 @@ library(XLConnect)
 rm(list = ls(all = TRUE))
 
 # Define folders and create if needed
-rawdatafolder <- "raw_data"
+rawdatafolder <- "KGS_downloaded_data"
 if (!file.exists(rawdatafolder)) dir.create(rawdatafolder)
-processeddatafolder <- "processed_data"
+processeddatafolder <- "KGS_processed_data"
 if (!file.exists(processeddatafolder)) dir.create(processeddatafolder)
 
 # Data to 2009 is provided in xls format
@@ -41,27 +44,28 @@ for (decade in xlsx_decades) {
 # Download data from KGS website and save in
 
 # All data prior to 1950 is in a single file called history.xls
-download.file("http://www.kgs.ku.edu/PRS/County/history.xls", destfile="raw_data/history.xls", method="wget", quiet = FALSE, mode = "w",cacheOK = TRUE, extra = getOption("download.file.extra"))
+dest <- file.path(rawdatafolder, "history.xls")
+download.file("http://www.kgs.ku.edu/PRS/County/history.xls", destfile=dest, method="wget", quiet = FALSE, mode = "w",cacheOK = TRUE, extra = getOption("download.file.extra"))
 
 # All data after 1950 is in a single file per decade
 for (fname in fnames) {
-        url=paste("http://www.kgs.ku.edu/PRS/County/", fname, sep="")
-        dest=file.path(rawdatafolder, fname)
+        url <- paste("http://www.kgs.ku.edu/PRS/County/", fname, sep="")
+        dest <- file.path(rawdatafolder, fname)
         download.file(url, destfile=dest, method="wget", quiet = FALSE, mode = "w",cacheOK = TRUE, extra = getOption("download.file.extra"))
 }
 
 # Load downloaded data up to 1950 into a dataframe
-history.df <- readWorksheetFromFile("raw_data/history.xls", sheet=1)
-names(history.df)[names(history.df)=="YEAR"] <- "Year"
-
-history.df$OIL_PROD <- history.df$X.OIL #+ history.df$X.UNATTRIB_OIL
-history.df$GAS_PROD <- history.df$X.GAS #+ history.df$X.UNATTRIB_GAS
-history.df$OIL_WELLS <- NA
-history.df$GAS_WELLS <- NA
-history.df$COUNTY <- "Statewide"
-history.df <- dplyr::select(history.df, COUNTY, Year, OIL_PROD, GAS_PROD)
-history.df <- dplyr::filter(history.df, as.numeric(Year) < 1950)
-history.df <- tidyr::gather(history.df, "Measure", "Value", -COUNTY, -Year)
+fileToGet <- file.path(rawdatafolder, "history.xls")
+history.df <- readWorksheetFromFile(fileToGet, sheet=1)
+names(history.df)[names(history.df)=="YEAR"] <- "Year"  # Change case to agree with post-1950 data
+history.df$OIL_PROD <- history.df$X.OIL  #+ history.df$X.UNATTRIB_OIL
+history.df$GAS_PROD <- history.df$X.GAS  #+ history.df$X.UNATTRIB_GAS
+history.df$OIL_WELLS <- NA  # No data before 1950 but include column for later merge
+history.df$GAS_WELLS <- NA  # No data before 1950 but include column for later merge
+history.df$COUNTY <- "Statewide"  # No data by county before 1950
+history.df <- dplyr::select(history.df, COUNTY, Year, OIL_PROD, GAS_PROD)  # Drop redundant columns
+history.df <- dplyr::filter(history.df, as.numeric(Year) < 1950)  # Drop data from after 1950. Will be replaced by county data
+history.df <- tidyr::gather(history.df, "Measure", "Value", -COUNTY, -Year)  # Reshape to long format
 history.df$Value <- as.numeric(history.df$Value)
 history.df$Year <- as.character(history.df$Year)
 history.df$COUNTY <- as.character(history.df$COUNTY)
@@ -70,32 +74,36 @@ KS_oil_pre1950.df <- history.df
 
 # Load data from post 1950
 KS_oil_post1950.df <- data.frame(COUNTY=character())
-
 for (fname in fnames) {
-        fileToGet=file.path(rawdatafolder, fname)
+        fileToGet <- file.path(rawdatafolder, fname)
         df <- readWorksheetFromFile(fileToGet, sheet=1)
         df$COUNTY <- as.character(df$COUNTY)
-        df$COUNTY <- gsub("State Total", "Statewide", df$COUNTY) # Switched from Statewide to State Total in 1970
-        df$COUNTY <- gsub("Mcpherson", "McPherson", df$COUNTY) # Fix case
+        df$COUNTY <- gsub("State Total", "Statewide", df$COUNTY)  # Switched from Statewide to State Total in 1970
+        df$COUNTY <- gsub("Mcpherson", "McPherson", df$COUNTY)  # Fix case
         KS_oil_post1950.df <- dplyr::full_join(KS_oil_post1950.df, df, by="COUNTY")
 }
 
-KS_oil_post1950.df <- dplyr::filter(KS_oil_post1950.df, !is.na(COUNTY)) # Remove rows where no county name
-KS_counties <- KS_oil_post1950.df$COUNTY
+KS_oil_post1950.df <- dplyr::filter(KS_oil_post1950.df, !is.na(COUNTY))  # Remove rows where no county name
+KS_counties <- KS_oil_post1950.df$COUNTY  # Extract a list of KS oil and gas producing counties
 
 
 # Tidy the data into a long format
-KS_oil_post1950.df <- tidyr::gather(KS_oil_post1950.df, "Metric", "Value", -1)
-KS_oil_post1950.df <- tidyr::separate(KS_oil_post1950.df, "Metric", into=c("Year", "Measure"), sep="_", extra="merge", remove=TRUE)
-KS_oil_post1950.df$Year <- stringr::str_sub(as.character(KS_oil_post1950.df$Year), -4, -1)
+KS_oil_post1950.df <- tidyr::gather(KS_oil_post1950.df, "Metric", "Value", -COUNTY)  # Reshape to long format
+KS_oil_post1950.df <- tidyr::separate(KS_oil_post1950.df, "Metric", into=c("Year", "Measure"), sep="_", extra="merge", remove=TRUE)  # e.g. "X1950_OIL_PROD" to "X1950" + "OIL_PROD"
+KS_oil_post1950.df$Year <- stringr::str_sub(as.character(KS_oil_post1950.df$Year), -4, -1)  # Extract year from e.g. "X1950"  to "1950"
 KS_oil_post1950.df$COUNTY <- as.factor(KS_oil_post1950.df$COUNTY)
 KS_oil_post1950.df$Value <- as.numeric(KS_oil_post1950.df$Value)
 KS_oil_post1950.df$Measure <- as.factor(KS_oil_post1950.df$Measure)
+
+# Join pre and post 1950 data into one data frame
 KS_oil.df <- dplyr::full_join(KS_oil_pre1950.df, KS_oil_post1950.df, by=c("COUNTY", "Year", "Measure", "Value"))
 
 # Save the combined data as a csv and R files
-dest=file.path(processeddatafolder, "KS_oil_and_gas.csv")
+dest=file.path(processeddatafolder, "KS_oil_and_gas_annual_production.csv")
 write.csv(KS_oil.df,dest,row.names=FALSE)
 
-dest=file.path(processeddatafolder, "KS_oil_and_gas.R")
+dest=file.path(processeddatafolder, "KS_oil_and_gas_producing_counties.csv")
+write.csv(KS_counties, dest,row.names=FALSE)
+
+dest=file.path(processeddatafolder, "KS_oil_and_gas_annual_production_data.R")
 dump(c("KS_oil.df", "KS_counties"),file=dest)
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# ks_oil_gas
-Kansas Oil &amp; Gas Data Downloader
+# Kansas Oil &amp; Gas Data Downloader 
 
-An R script to download and combine annual data for Kansas oil and gas production and well counts from the Kansas Geological Survey at http://www.kgs.ku.edu/PRS/petro/state.html
+ks_oil_gas.R is an R script to download and combine annual data for Kansas oil and gas production and well counts from the Kansas Geological Survey at http://www.kgs.ku.edu/PRS/petro/state.html
+
+The output is saved in a data frame and csv file in long format for ease of plotting in R. The script requires XLConnect, dplyr, tidyr and stringr.
