b327f958e9cde6b488a1b3fae9039f008b264090
Stephen Johnson <stephenjjohnson@gmail.com>
2016-04-25 09:17:35 -0500

    Added GPL-3 license

--- R/ks_oil_gas.R
+++ R/ks_oil_gas.R
@@ -1,10 +1,24 @@
-# Download and combine annual data for oil and gas production from
-# the Kansas Geological Survey at http://www.kgs.ku.edu/PRS/petro/state.html
+#       Download and combine annual data for KS oil and gas production
+#       Copyright (C) 2016 Stephen Johnson
 #
-# Saves both original files from the website and the processed data.
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
 #
-# Author: Stephen Johnson
-# Licence: GPL-2
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+#       Download and combine annual data for oil and gas production from
+#       the Kansas Geological Survey at http://www.kgs.ku.edu/PRS/petro/state.html
+#       then saves both original files from the website and the processed data.
+
 
 
 # load libraries
